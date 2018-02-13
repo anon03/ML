5dd5c94d5782424b9e8f059cf791f3c0dfeb059c
anon03 <gurung.amir3@gmail.com>
2018-02-13 09:35:32 +0545

    Initial commit

---
+++ CoreMLImageDetection/ViewController.swift
@@ -0,0 +1,53 @@
+]
+
+import UIKit
+
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet var myPhoto: UIImageView!
+    @IBOutlet var lblResult: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+    
+    @IBAction func takePhoto(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func pickImage(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            myPhoto.contentMode = .scaleToFill
+            myPhoto.image = pickedImage
+        }
+        picker.dismiss(animated: true, completion: nil)
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CoreMLImageDetectionTests/CoreMLImageDetectionTests.swift
@@ -0,0 +1,26 @@
+
+
+import XCTest
+@testable import CoreMLImageDetection
+
+class CoreMLImageDetectionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        
+    }
+    
+    func testPerformanceExample() {
+       
+        self.measure {
+        }
+    }
+    
+}
